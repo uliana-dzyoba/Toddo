a478da9246012cbc7586575269dde3ea57b90f9a
Uliana Dzyoba <ulianadzyoba@gmail.com>
2020-12-16 16:18:55 +0200

    commit 2

--- Toddo/app/src/main/java/com/example/toddo/ui/gallery/GalleryFragment.java
+++ Toddo/app/src/main/java/com/example/toddo/ui/gallery/GalleryFragment.java
@@ -24,13 +24,13 @@ import java.util.Locale;
 public class GalleryFragment extends Fragment {
 
     private GalleryViewModel galleryViewModel;
-//    private static final long START_TIME_IN_MILLIS = 600000;
-//    private TextView mTextViewCountDown;
-//    private Button mButtonStartPause;
-//    private Button mButtonReset;
-//    private CountDownTimer mCountDownTimer;
-//    private boolean mTimerRunning;
-//    private long mTimeLeftInMillis = START_TIME_IN_MILLIS;
+    private static final long START_TIME_IN_MILLIS = 600000;
+    private TextView mTextViewCountDown;
+    private Button mButtonStartPause;
+    private Button mButtonReset;
+    private CountDownTimer mCountDownTimer;
+    private boolean mTimerRunning;
+    private long mTimeLeftInMillis = START_TIME_IN_MILLIS;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
@@ -44,26 +44,26 @@ public class GalleryFragment extends Fragment {
                 textView.setText(s);
             }
         });
-//        mTextViewCountDown = root.findViewById(R.id.text_view_countdown);
-//        mButtonStartPause = root.findViewById(R.id.button_start_pause);
-//        mButtonReset = root.findViewById(R.id.button_reset);
-//        mButtonStartPause.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View v) {
-//                if (mTimerRunning) {
-//                    pauseTimer();
-//                } else {
-//                    startTimer();
-//                }
-//            }
-//        });
-//        mButtonReset.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View v) {
-//                resetTimer();
-//            }
-//        });
-//        updateCountDownText();
+        mTextViewCountDown = root.findViewById(R.id.text_view_countdown);
+        mButtonStartPause = root.findViewById(R.id.button_start_pause);
+        mButtonReset = root.findViewById(R.id.button_reset);
+        mButtonStartPause.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (mTimerRunning) {
+                    pauseTimer();
+                } else {
+                    startTimer();
+                }
+            }
+        });
+        mButtonReset.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                resetTimer();
+            }
+        });
+        updateCountDownText();
         return root;
     }
 
@@ -93,41 +93,41 @@ public class GalleryFragment extends Fragment {
 //        updateCountDownText();
 //    }
 
-//    private void startTimer() {
-//        mCountDownTimer = new CountDownTimer(mTimeLeftInMillis, 1000) {
-//            @Override
-//            public void onTick(long millisUntilFinished) {
-//                mTimeLeftInMillis = millisUntilFinished;
-//                updateCountDownText();
-//            }
-//            @Override
-//            public void onFinish() {
-//                mTimerRunning = false;
-//                mButtonStartPause.setText("Start");
-//                mButtonStartPause.setVisibility(View.INVISIBLE);
-//                mButtonReset.setVisibility(View.VISIBLE);
-//            }
-//        }.start();
-//        mTimerRunning = true;
-//        mButtonStartPause.setText("pause");
-//        mButtonReset.setVisibility(View.INVISIBLE);
-//    }
-//    private void pauseTimer() {
-//        mCountDownTimer.cancel();
-//        mTimerRunning = false;
-//        mButtonStartPause.setText("Start");
-//        mButtonReset.setVisibility(View.VISIBLE);
-//    }
-//    private void resetTimer() {
-//        mTimeLeftInMillis = START_TIME_IN_MILLIS;
-//        updateCountDownText();
-//        mButtonReset.setVisibility(View.INVISIBLE);
-//        mButtonStartPause.setVisibility(View.VISIBLE);
-//    }
-//    private void updateCountDownText() {
-//        int minutes = (int) (mTimeLeftInMillis / 1000) / 60;
-//        int seconds = (int) (mTimeLeftInMillis / 1000) % 60;
-//        String timeLeftFormatted = String.format(Locale.getDefault(), "%02d:%02d", minutes, seconds);
-//        mTextViewCountDown.setText(timeLeftFormatted);
-//    }
+    private void startTimer() {
+        mCountDownTimer = new CountDownTimer(mTimeLeftInMillis, 1000) {
+            @Override
+            public void onTick(long millisUntilFinished) {
+                mTimeLeftInMillis = millisUntilFinished;
+                updateCountDownText();
+            }
+            @Override
+            public void onFinish() {
+                mTimerRunning = false;
+                mButtonStartPause.setText("Start");
+                mButtonStartPause.setVisibility(View.INVISIBLE);
+                mButtonReset.setVisibility(View.VISIBLE);
+            }
+        }.start();
+        mTimerRunning = true;
+        mButtonStartPause.setText("pause");
+        mButtonReset.setVisibility(View.INVISIBLE);
+    }
+    private void pauseTimer() {
+        mCountDownTimer.cancel();
+        mTimerRunning = false;
+        mButtonStartPause.setText("Start");
+        mButtonReset.setVisibility(View.VISIBLE);
+    }
+    private void resetTimer() {
+        mTimeLeftInMillis = START_TIME_IN_MILLIS;
+        updateCountDownText();
+        mButtonReset.setVisibility(View.INVISIBLE);
+        mButtonStartPause.setVisibility(View.VISIBLE);
+    }
+    private void updateCountDownText() {
+        int minutes = (int) (mTimeLeftInMillis / 1000) / 60;
+        int seconds = (int) (mTimeLeftInMillis / 1000) % 60;
+        String timeLeftFormatted = String.format(Locale.getDefault(), "%02d:%02d", minutes, seconds);
+        mTextViewCountDown.setText(timeLeftFormatted);
+    }
 }
